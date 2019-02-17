10f8d388c4e7e22c59df09d576e6a7a28ade17fd
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2019-02-17 18:04:33 +0530

    Add files via upload

---
+++ grizzly_stores/scripts/username.js
@@ -0,0 +1,16 @@
+function fn(){
+document.location.reload();
+}
+function fn1(){
+	alert("Items added");
+	document.location.reload();
+}
+function closeTab(){
+	var v=confirm("Do you want to logout???");
+	if(v==true)
+	window.close();
+}
+function openViewProduct(){
+	window.open("file:///C:/Users/765633/Desktop/practice_check/viewProduct.html");
+}
+
